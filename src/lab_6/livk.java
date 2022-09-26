91fd353554e3bedbc000b1794c13ab92a3d20fcc
MININT-VC5T29R-local <thanhvinh231198@gmail.com>
2022-09-26 17:55:02 +0700

    Lab_6

--- src/lab_6/livk.java
+++ src/lab_6/livk.java
@@ -3,9 +3,13 @@ package lab_6;
 public class livk {
     public static void main(String[] args) {
         String chuoi= "https://google.com";
-        String s1="https";
+        String s1="https:";
+        String s3="http:";
         String s2=".com";
+        String s4=".net";
         System.out.println(chuoi.contains(s1));
         System.out.println(chuoi.contains(s2));
+        System.out.println(chuoi.contains(s3));
+        System.out.println(chuoi.contains(s4));
     }
 }
