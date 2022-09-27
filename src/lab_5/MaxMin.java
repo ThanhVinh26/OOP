25647e75fa66f7e542d9e87e262d24dfed4e7ff0
MININT-VC5T29R-local <thanhvinh231198@gmail.com>
2022-09-27 10:00:02 +0700

    Lab_5

--- src/lab_5/MaxMin.java
+++ src/lab_5/MaxMin.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 public class MaxMin {
     public static void main(String[] args) {
         ArrayList<Integer> arrList = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
         int number;
+        arrList.add(1);
+        arrList.add(5);
+        arrList.add(7);
+        arrList.add(1);
+        arrList.add(9);
+        // hiển thị các phần tử của list
+        //System.out.print("Nhập số phần tử của Array: ");
+        //int n = scanner.nextInt();
 
-        System.out.print("Nhập số phần tử của Array: ");
-        int n = scanner.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Nhập phần tử thứ " + i + ": ");
-            number = scanner.nextInt();
-            arrList.add(number);
-        }
-
+        //for (int i = 0; i < mang; i++) {
+            //System.out.print("Nhập phần tử thứ " + i + ": ");
+            //number = scanner.nextInt();
+            //arrList.add(number);
+        //}
         int max = arrList.get(0);
         for (int i = 1; i < arrList.size(); i++) {
             if (arrList.get(i).compareTo(max) > 0) {
@@ -30,6 +34,8 @@ public class MaxMin {
                 min = arrList.get(i);
             }
         }
+        System.out.println("Các phần tử có trong list là: " );
+        System.out.println(arrList + "\n");
         System.out.println("Phần tử lớn nhất trong arr = " + max);
         System.out.println("Phần tử bé nhất trong arr = " + min);
     }
